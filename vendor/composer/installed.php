72a96e4b0667609b51e28c169e90b1173141a0cd
= <ftr.azka2015@gmail.com>
2026-09-18 15:35:25 +0700

    perbaikan direct cctv

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'codeigniter4/appstarter',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '2bda326c956cb7e0217740510d84adc9e9445bcd',
+        'reference' => 'be490562c363c4d0f6b7e7060e702cca55bf6c44',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'codeigniter4/appstarter' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '2bda326c956cb7e0217740510d84adc9e9445bcd',
+            'reference' => 'be490562c363c4d0f6b7e7060e702cca55bf6c44',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
